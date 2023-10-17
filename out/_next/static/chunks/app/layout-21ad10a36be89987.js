f1b068b66b307c7fdd4882293f412cc6df7fbff0
Saman <samangithoseinpour@gmail.com>
2023-10-17 19:15:51 +0330

    production

---
+++ out/_next/static/chunks/app/layout-21ad10a36be89987.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{4630:function(e,n,t){Promise.resolve().then(t.t.bind(t,8877,23)),Promise.resolve().then(t.t.bind(t,6964,23)),Promise.resolve().then(t.t.bind(t,4724,23))},8877:function(){}},function(e){e.O(0,[929,971,864,744],function(){return e(e.s=4630)}),_N_E=e.O()}]);
