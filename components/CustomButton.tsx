d794efae7523a465cc800ffeb99239a67e2c51c2
Saman <samangithoseinpour@gmail.com>
2023-10-17 11:38:51 +0330

    create components structure and design hero header section

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -4,7 +4,7 @@ type CustomButtonProps = {
   type: 'button' | 'submit';
   title: string;
   icon?: string;
-  variant: 'btn_dark_green';
+  variant: 'btn_dark_green' | 'btn_green' | 'btn_white_text';
 };
 
 const CustomButton = ({ type, title, icon, variant }: CustomButtonProps) => {
@@ -14,7 +14,9 @@ const CustomButton = ({ type, title, icon, variant }: CustomButtonProps) => {
       className={`${variant} flexCenter gap-3 rounded-full border`}
     >
       {icon && <Image src={icon} alt="icon" width={24} height={24} />}
-      <label>{title}</label>
+      <label className="bold-16 whitespace-nowrap cursor-pointer">
+        {title}
+      </label>
     </button>
   );
 };
--- components/index.ts
+++ components/index.ts
@@ -1,4 +1,10 @@
 import Footer from './Footer';
 import Navbar from './Navbar';
+import CustomButton from './CustomButton';
+import Hero from './Hero';
+import Camp from './Camp';
+import Guide from './Guide';
+import Features from './Features';
+import GetApp from './GetApp';
 
-export { Footer, Navbar };
+export { Footer, Navbar, CustomButton, Hero, Camp, Guide, Features, GetApp };
