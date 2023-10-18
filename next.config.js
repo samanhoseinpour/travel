8af688ab9332a35c8a619fb2b8823cbff2fc5575
Saman <samangithoseinpour@gmail.com>
2023-10-18 19:22:04 +0330

    production-v.0.0.1

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
+const withPWA = require('next-pwa')({
+  dest: 'public',
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
 };
 
-module.exports = nextConfig;
+/** @type {import('next').NextConfig} */
+
+module.exports = withPWA({
+  nextConfig,
+});
